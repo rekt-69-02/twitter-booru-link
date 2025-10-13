de640268b3bf3eb414adeda8ad4c7c32d34a2377
tker_96 <bluezenithlol@gmail.com>
2025-10-13 23:10:04 +0800

    l

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import json, os, subprocess, bs4
-from saucenao_api import SauceNao
+import json, os, subprocess, bs4, pathlib
 import selenium, urllib.parse, time
 from selenium import webdriver
 from selenium_stealth import stealth
@@ -11,8 +10,6 @@ from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.core.os_manager import ChromeType
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
-from . import urm
-from .urm import SauceNaoResult
 
 options = Options()
 options.add_argument("start-maximized")
@@ -21,7 +18,7 @@ options.add_argument('--no-sandbox')
 options.add_argument('--disable-dev-shm-usage')
 options.add_experimental_option("excludeSwitches", ["enable-automation"])
 options.add_experimental_option('useAutomationExtension', False)
-driver = webdriver.Chrome(executable_path=ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install(), options=options)
+driver = webdriver.Chrome(service=Service(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()), options=options)
 valiable_image_extensions = ["jpg", "jpeg", "png", "webp"]
 headers = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/140.0.0.0 Safari/537.36"}
 
@@ -72,38 +69,92 @@ def get_artist_url_yandere(url):
 def search_saucenao(username, image):
     driver.get("https://saucenao.com/")
     upload = driver.find_element(By.NAME, "file")
-    upload.send_keys(f"/home/user/git repo/x-booru-linker/gallery-dl/twitter/{username}/{image}")
+    upload.send_keys(f"{pathlib.Path().resolve()}/gallery-dl/twitter/{username}/{image}")
     driver.find_element(By.CSS_SELECTOR, "input[type='submit']").click()
+
+def download_10_images(twitter_media_url):
+    gallery_dl_command = ["gallery-dl", "--range", "21-30", "-d", "./gallery-dl/", "--cookies-from-browser", "vivaldi"]
+    gallery_dl_command.append(twitter_media_url)
+    subprocess.run(gallery_dl_command)
+
+def extract_result(html_source):
+    b = bs4.BeautifulSoup(html_source, "html.parser")
+    results = b.find_all("div", {"class": "result"})
+    i = None
+    for result in results:
+        try:
+            if result.attrs["id"] == "result-hidden-notification":
+                i = results.index(result)
+        except KeyError:
+            continue
+    
+    try:
+        while results[i]:
+            results.pop(i)
+    except IndexError:
+        pass
+
+    if results == []:
+        return results
+
+    for result in results:
+             
+
+class SauceNaoResult():
+    def __init__(self, raw: bs4.Tag):
+        self.raw:        dict = raw
+        self.similarity: float = float(self._get_similarity(raw))
+        self.urls:       list[str] = self._get_urls(raw)
+
+    def _get_similarity(self, raw: bs4.Tag) -> float:
+        s = raw.find("div", {"class": "resultsimilarityinfo"})
+        return s.text
+    
+    def _get_urls(self, raw: bs4.Tag) -> list:
+        urls = []
+        r = raw.find("div", {"class": "resultmiscinfo"})
+        for u in r.find_all("a"):
+            urls.append(u.attrs["href"])
+        return urls
 
 def main():
     twitter_followings_json_file = input("please input file path:")
     twitter_followings = [x for x in json.load(open(twitter_followings_json_file, 'r', encoding="utf8"))]
 
     for following in twitter_followings:
-        urm.download_10_images(twitter_media_url=following["url"] + "/media/")
-        file_path = f"/home/user/git repo/x-booru-linker/gallery-dl/{following["screen_name"]}/"
-            
+        download_10_images(twitter_media_url=following["url"] + "/media/")
+        file_path = f"{pathlib.Path().resolve()}/gallery-dl/twitter/{following["screen_name"]}/"
+
+        o = {
+                "twitter_url": following["url"]
+            }
+        
         for img in os.listdir(file_path):
             search_saucenao(username=following["screen_name"], image=img)
             raw_html = driver.page_source
-            results = urm.extract_result(raw_html)            
+            results = extract_result(raw_html)            
             sauces = []
             for result in results:
                 sauces.append(SauceNaoResult(result))
 
-            o = {
-                "twitter_url": following["url"]
-            }
+            if o.get("danbooru_url") and o.get("gelbooru_url") and o.get("yandere_url"):
+                break
 
             for sauce in sauces:
                 for url in sauce.urls:
-                    if "danbooru.donmai.us" in url:
+                    if "danbooru.donmai.us" in url and not o.get("danbooru_url"):
                         o["danbooru_url"] = get_artist_url_danbooru(url)
                     
-                    if "gelbooru.com" in url:
+                    if "gelbooru.com" in url and not o.get("gelbooru_url"):
                         o["gelbooru_url"] = get_artist_url_gelbooru(url)
 
-                    if "yande.re" in url:
+                    if "yande.re" in url and not o.get("yandere_url"):
                         o["yandere_url"] = get_artist_url_yandere(url)
 
-            
+            with open("output.json", "w+") as file:
+                j = json.load(file)
+                j[following["screen_name"]] = o
+                json.dump(j, file)
+
+if __name__ == "__main__":
+    main()
